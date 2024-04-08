cab0e19e182cae7e4b1a663b5e5f1dd3cd9063cf
MaffyMaths <matvey33.baryshev@mail.ru>
2024-04-08 11:56:52 +0300

    logic for settings configuration

--- src/main/java/com/example/custommaplayersbuilder/DialogController.java
+++ src/main/java/com/example/custommaplayersbuilder/DialogController.java
@@ -1,6 +1,7 @@
 package com.example.custommaplayersbuilder;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -9,12 +10,26 @@ import java.io.File;
 
 public class DialogController {
     private Converter converter;
+
+    @FXML
+    private Button pathSelect;
+    @FXML
+    private Button convertAllButton;
+    @FXML
+    private Button convertLineButton;
+    @FXML
+    private Button convertPolygonButton;
+    @FXML
+    private Button convertPointsButton;
+    @FXML
+    private Button convertRouteButton;
+
     private Window window;
     private double[][] currentRoute;
     private double[][] currentLine;
     private double[][] currentPolygon;
     private double[][] currentPoints;
-    private String selectedPath;
+
 
     public void initData(double[][] currentRoute, double[][] currentLine,
                          double[][] currentPolygon, double[][] currentPoints) {
@@ -24,17 +39,13 @@ public class DialogController {
         this.currentPoints = currentPoints;
     }
 
-    public void setConverter(Converter converter) {
-        this.converter = converter;
-    }
-
     public void setWindow(Window window) {
         this.window = window;
     }
 
     @FXML
     private void convertAll() {
-        converter.allToFeatureCollection(currentRoute, currentLine, currentPolygon, currentPoints, selectedPath);
+        converter.allToFeatureCollection(currentRoute, currentLine, currentPolygon, currentPoints);
     }
 
     @FXML
@@ -74,7 +85,27 @@ public class DialogController {
         File file = fileChooser.showSaveDialog(stage);
 
         if (file != null) {
-            selectedPath = file.getAbsolutePath();
+            String selectedPath = file.getAbsolutePath();
+
+            this.converter = new Converter(selectedPath);
+            pathSelect.setText(selectedPath);
+
+            boolean any = false;
+            if (currentLine.length > 0) {
+                convertLineButton.setDisable(false); any = true;
+            }
+            if (currentPolygon.length > 0) {
+                convertPolygonButton.setDisable(false); any = true;
+            }
+            if (currentPoints.length > 0) {
+                convertPointsButton.setDisable(false); any = true;
+            }
+            if (currentRoute.length > 0) {
+                convertRouteButton.setDisable(false); any = true;
+            }
+            if (any) {
+                convertAllButton.setDisable(false);
+            }
         }
     }
 }
