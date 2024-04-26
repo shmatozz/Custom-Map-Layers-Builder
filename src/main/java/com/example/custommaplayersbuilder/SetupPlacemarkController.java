335095c8f20f7b3c55aef3e659f718511139f7b7
MaffyMaths <matvey33.baryshev@mail.ru>
2024-04-26 12:56:57 +0300

    add custom placemark setup dialog window

---
+++ src/main/java/com/example/custommaplayersbuilder/SetupPlacemarkController.java
@@ -0,0 +1,57 @@
+package com.example.custommaplayersbuilder;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import org.json.JSONObject;
+
+public class SetupPlacemarkController {
+    private double[] points;
+
+    @FXML
+    public Button createPlacemark;
+    @FXML
+    public TextField headerInput;
+    @FXML
+    public TextField textInput;
+    @FXML
+    public TextField hintInput;
+    @FXML
+    public ColorPicker colorPicker;
+
+    private Stage stage;
+    private WebView webView;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void setWebView(WebView webView) {
+        this.webView = webView;
+    }
+
+    public void setPoints(double[] points) {
+        this.points = points;
+    }
+
+    public JSONObject getEnteredData() {
+        JSONObject data = new JSONObject();
+
+        data.put("coords", points);
+        data.put("header", headerInput.getText());
+        data.put("body", textInput.getText());
+        data.put("hint", hintInput.getText());
+        data.put("color", colorPicker.getValue().toString());
+
+        return data;
+    }
+
+    @FXML
+    public void createPlacemark() {
+        stage.close();
+    }
+}
